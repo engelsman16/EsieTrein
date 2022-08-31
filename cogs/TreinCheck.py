539f128f6ba5e17f7a530a4ed6f8e18b07220d92
engelsman16 <teun.engels@gmail.com>
2022-08-31 20:40:00 +0200

    fixed daily cycle

--- cogs/TreinCheck.py
+++ cogs/TreinCheck.py
@@ -2,48 +2,59 @@ import json
 import discord
 from discord.ext import commands, tasks
 import http.client, urllib.request, urllib.parse, urllib.error, base64
+import datetime
+
+from config import settings
+
 
 class TreinCheck(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.check.start()
-        
+
     def cog_unload(self):
-      self.check.cancel()
-    
+        self.check.cancel()
+
     @commands.Cog.listener()
     async def on_ready_cock(self):
-      print("TreinCheck is ready")
+        print("TreinCheck is online")
 
     @tasks.loop(seconds=10)
     async def check(self):
+        time = datetime.datetime.now() + datetime.timedelta(days=1)
         headers = {
-          # Request headers
-          'Ocp-Apim-Subscription-Key': 'f299fb5b81e8454bade4f07770d1b5e5',
+            # Request headers
+            "Ocp-Apim-Subscription-Key": settings.APIKEY,
         }
-        
-        params = urllib.parse.urlencode({
-          # Request parameters
-          'originUicCode': '8400339',
-          'destinationUicCode': '8400206',
-          'dateTime': '2022-09-01T08:00',
-        })
-        
+
+        params = urllib.parse.urlencode(
+            {
+                # Request parameters
+                "originUicCode": "8400339",
+                "destinationUicCode": "8400206",
+                "dateTime": time.strftime("%Y-%m-%d") + "T08:00",
+            }
+        )
+
         try:
-          conn = http.client.HTTPSConnection('gateway.apiportal.ns.nl')
-          conn.request("GET", "/reisinformatie-api/api/v3/trips?%s" % params, "{body}", headers)
-          response = conn.getresponse()
-          data = response.read().decode('utf-8')
-          dataobj = json.loads(data)
-          print(dataobj["trips"][0]['status'])
-          conn.close()
+            conn = http.client.HTTPSConnection("gateway.apiportal.ns.nl")
+            conn.request("GET", "/reisinformatie-api/api/v3/trips?%s" % params, "{body}", headers)
+            response = conn.getresponse()
+            data = response.read().decode("utf-8")
+            dataobj = json.loads(data)
+
+            train_status = dataobj["trips"][0]["status"]
+            await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=train_status))
+            conn.close()
+
         except Exception as e:
-          print(e)
+            print(e)
 
     @check.before_loop
     async def before_check(self):
-        print('waiting...')
+        print("waiting...")
         await self.bot.wait_until_ready()
+
 
 async def setup(bot):
     await bot.add_cog(TreinCheck(bot))
--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     TOKEN: str
     APPID: int
+    APIKEY: str
 
     class Config:
         env_file = ".env"
